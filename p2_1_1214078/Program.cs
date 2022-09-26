a6f15aceca384101247bad5ef2459f83a9377d50
LANA <you@example.com>
2022-09-26 16:00:12 +0700

    Add project files.

---
+++ p2_1_1214078/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2_1_1214078
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Angka Pertama = ");
+            int angka1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Angka Kedua = ");
+            int angka2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(angka1 + angka2);
+            Console.WriteLine(angka1 - angka2);
+            Console.WriteLine(angka1 * angka2);
+            Console.WriteLine(angka1 / angka2);
+        }
+    }
+}
