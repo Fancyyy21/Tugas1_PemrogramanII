4866b016bf635ed5a3801964ef66d85ec64ce09c
LANA <imanulhaq5@gmail.com>
2022-09-26 16:13:48 +0700

    Ini fix

--- p2_1_1214078/Program.cs
+++ p2_1_1214078/Program.cs
@@ -10,16 +10,35 @@ namespace p2_1_1214078
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Silakan Masukkan Perhitungan Disini !");
+
             Console.Write("Angka Pertama = ");
             int angka1 = int.Parse(Console.ReadLine());
 
             Console.Write("Angka Kedua = ");
             int angka2 = int.Parse(Console.ReadLine());
 
+           
+            Console.Write(angka1);
+            Console.Write(" + ");
+            Console.Write(angka2 + " = ");
             Console.WriteLine(angka1 + angka2);
+
+            Console.Write(angka1);
+            Console.Write(" - ");
+            Console.Write(angka2 + " = ");
             Console.WriteLine(angka1 - angka2);
+
+            Console.Write(angka1);
+            Console.Write(" x ");
+            Console.Write(angka2 + " = ");
             Console.WriteLine(angka1 * angka2);
+
+            Console.Write(angka1);
+            Console.Write(" : ");
+            Console.Write(angka2 + " = ");
             Console.WriteLine(angka1 / angka2);
+
         }
     }
 }
